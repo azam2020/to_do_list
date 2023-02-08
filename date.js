c374021da7c6492fed03f25ab0ce3fcf2bbf8a1d
Mohammad Azam <azam8933033@gmail.com>
2023-02-08 20:14:38 +0530

    first update

---
+++ date.js
@@ -0,0 +1,20 @@
+
+exports.getDate = function() {
+ const date = new Date();
+ const options = {
+     weekday: "long",
+     day: "numeric",
+     month: "long"
+ }
+ let day = date.toLocaleDateString("en-US",options);
+ return day;
+}
+
+exports.getDay = function (){
+    const date = new Date();
+    const options = {
+        weekday: "long",
+    }
+    let day = date.toLocaleDateString("en-US",options);
+    return day;
+   }
